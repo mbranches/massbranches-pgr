0ff5e23d3aaf762c650bc6a0f8b5571f4aa5ddb7
mbranches <marcusvbranches@gmail.com>
2026-07-15 11:55:12 -0300

    feat: start project

---
+++ pgr-api/src/test/java/com/massbranches/pgr_api/PgrApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.massbranches.pgr_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgrApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
